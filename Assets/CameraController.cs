9c1a26b6919265df9eb499e371e50f403785eb4c
Scott Millar <scottmillar95@Gmail.com>
2016-05-31 18:51:30 +0100

    Basic Movement Implemented

---
+++ Assets/CameraController.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour
+{
+
+    public Transform target;
+    public float cameraSmooth = 0.09f;
+    public Vector3 offsetFromTarget = new Vector3(0, 6, -8);
+    public float xTilt = 10;
+
+    Vector3 destination = Vector3.zero;
+    CharacterController charController;
+
+    float rotateSpeed = 0;
+
+    // Use this for initialization
+    void Start()
+    {
+        setTarget(target);
+    }
+
+    public void setTarget(Transform t)
+    {
+        target = t;
+
+        if (target != null)
+        {
+            if (target.GetComponent<CharacterController>())
+            {
+                charController = target.GetComponent<CharacterController>();
+            }
+            else
+                Debug.LogError("Camera's target needs a character controller.");
+        }
+        else
+            Debug.LogError("setTarget has no targer.");
+
+    }
+
+    void LateUpdate()
+    {
+        MoveTo();
+
+        LookAt();
+    }
+
+    void MoveTo()
+    {
+        destination = charController.TargetRotation * offsetFromTarget;
+        destination += target.position;
+        transform.position = destination;
+    }
+
+    void LookAt()
+    {
+        float eulerYAngle = Mathf.SmoothDampAngle(transform.eulerAngles.y, target.eulerAngles.y, ref rotateSpeed, cameraSmooth);
+        transform.rotation = Quaternion.Euler(transform.eulerAngles.x, eulerYAngle, 0);
+    }
+
+}
+
---
+++ Assets/CharacterController.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterController : MonoBehaviour {
+
+
+    public float speed = 12;
+    public float rotateSpeed = 100;
+
+    Quaternion targetRotation;
+    Rigidbody rigidBody;
+    float forwardInput, turnInput;
+
+    Vector3 direction;
+
+
+    public Quaternion TargetRotation
+    {
+        get { return targetRotation; }
+    }
+
+	// Use this for initialization
+	void Start ()
+    {
+        //targetRotation = transform.rotation;
+        if (GetComponent<Rigidbody>())
+        {
+            rigidBody = GetComponent<Rigidbody>();
+        }
+        else
+            Debug.LogError("No rigidbody has been found");
+	}
+
+    void GetInput()
+    {
+        forwardInput = Input.GetAxis("Vertical");
+        turnInput = Input.GetAxis("Horizontal");
+
+        direction = new Vector3(turnInput, 0, forwardInput);
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        GetInput();
+        Turn();
+	}
+
+    void FixedUpdate()
+    {
+        Swim();
+    }
+
+    void Swim()
+    {
+        rigidBody.velocity =  direction * speed;
+       
+    }
+
+    void Turn()
+    {
+        if (direction != Vector3.zero)
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(-direction), Time.deltaTime * rotateSpeed);
+        }
+        else
+            Debug.LogError("Direction is = Vector3.zero");
+    }
+
+
+
+}
